f343c6df9278942e802b6aa7e82b1269fca67fdc
robin <robinleesuns13@gmail.com>
2019-08-17 14:11:05 -0700

    fix: correct customer id model name

--- Data/techtshirtContext.cs
+++ Data/techtshirtContext.cs
@@ -2,6 +2,7 @@ using techtshirt.Models;
 using Microsoft.EntityFrameworkCore;
 
 
+
 namespace techtshirt.Data
 {
     public class techtshirtContext : DbContext
@@ -20,6 +21,11 @@ namespace techtshirt.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+
+            // modelBuilder.Entity<Customer>()
+            //     .WithMany(c => c.Order)
+            //     .HasOne(e => e.Customer);
             // these two set the types for the decimal entity types for cost and sale_price
             modelBuilder.Entity<Inventory>()
                 .Property(p => p.cost)
@@ -29,34 +35,34 @@ namespace techtshirt.Data
                 .HasColumnType("decimal(16,4)");
 
             // Seed database
-            modelBuilder.Entity<Inventory>().HasData(
-                new Inventory
-                {
-                    id = 121,
-                    name = "Blue Tech",
-                    description = "Blue T-Shirt",
-                    size = "M",
-                    color = "blue",
-                    on_hand_qty = 44,
-                    cost = 2.50m,
-                    sale_price = 8.99m,
-                    allocated_qty = 9,
-                    total_qty = 80
-                },
-                new Inventory
-                {
-                    id = 100,
-                    name = "Black Tech",
-                    description = "Black T-Shirt",
-                    size = "XL",
-                    color = "violet",
-                    on_hand_qty = 24,
-                    cost = 2.99m,
-                    sale_price =4.99m,
-                    allocated_qty = 5,
-                    total_qty = 32
-                }
-            );
+            // modelBuilder.Entity<Inventory>().HasData(
+            //     new Inventory
+            //     {
+            //         id = 121,
+            //         name = "Blue Tech",
+            //         description = "Blue T-Shirt",
+            //         size = "M",
+            //         color = "blue",
+            //         on_hand_qty = 44,
+            //         cost = 2.50m,
+            //         sale_price = 8.99m,
+            //         allocated_qty = 9,
+            //         total_qty = 80
+            //     },
+            //     new Inventory
+            //     {
+            //         id = 100,
+            //         name = "Black Tech",
+            //         description = "Black T-Shirt",
+            //         size = "XL",
+            //         color = "violet",
+            //         on_hand_qty = 24,
+            //         cost = 2.99m,
+            //         sale_price =4.99m,
+            //         allocated_qty = 5,
+            //         total_qty = 32
+            //     }
+            // );
         }
 
     }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -8,14 +8,15 @@ namespace techtshirt.Models
 {
     public class Customer
     {
-        public int ID { get; set; }
+        public int id { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
         public string type { get; set; }
         public string address { get; set; }
-        
+
        // [DataType(DataType.PhoneNumber)] //view: "  @Html.EditorFor(model => model.PhoneNumber)  "
         public string phone { get; set; }
         public string company { get; set; }
+
     }
 }
