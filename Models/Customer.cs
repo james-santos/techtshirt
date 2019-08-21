090bce8f3ee659ab91e65fe6a5ff372cf6396d3b
robin <robinleesuns13@gmail.com>
2019-08-21 16:04:44 -0700

    feat: add data annotations to inv and customer model names

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,13 +9,19 @@ namespace techtshirt.Models
     public class Customer
     {
         public int id { get; set; }
+        [Display(Name = "First Name")]
         public string first_name { get; set; }
+        [Display(Name = "Last Name")]
         public string last_name { get; set; }
+        [Display(Name = "Type")]
         public string type { get; set; }
+        [Display(Name = "Address")]
         public string address { get; set; }
 
        // [DataType(DataType.PhoneNumber)] //view: "  @Html.EditorFor(model => model.PhoneNumber)  "
+        [Display(Name = "Phone Number")]
         public string phone { get; set; }
+        [Display(Name = "Company")]
         public string company { get; set; }
 
         public ICollection<Order> Order { get; set; }
--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -1,19 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace techtshirt.Models
 {
     // Inventory is configured via OnModelCreating
     public class Inventory
     {
         public int id { get; set; }
+        [Display(Name = "Item Name")]
         public string name { get; set; }
+        [Display(Name = "Description")]
         public string description { get; set; }
+        [Display(Name = "Size")]
         public string size { get; set; }
+        [Display(Name = "Color")]
         public string color { get; set; }
+        [Display(Name = "On Hand Quantity")]
         public int on_hand_qty { get; set; }
+        [Display(Name = "Cost")]
         public decimal cost { get; set; }
+        [Display(Name = "Sale Price")]
         public decimal sale_price { get; set; }
+        [Display(Name = "Allocated Quantity")]
         public int allocated_qty { get; set; }
+        [Display(Name = "Total Quantity")]
         public int total_qty { get; set; }
 
         public ICollection<Order_Inventory> Order_Inventory { get; set; }
