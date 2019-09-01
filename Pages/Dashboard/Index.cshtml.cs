96407ddc0a77c7a73bf887d1976593235e2d5893
robin <robinleesuns13@gmail.com>
2019-09-01 14:23:29 -0700

    feat: add query for orders made past 30 days and create table for order dashboard page

--- Pages/Dashboard/Index.cshtml.cs
+++ Pages/Dashboard/Index.cshtml.cs
@@ -20,14 +20,19 @@ namespace techtshirt.Pages.Dashboard
         }
 
         public IList<Order> Order { get;set; }
+        public IList<Order> PrevOrders { get;set; }
         public IList<Order_Inventory> Order_Inventory { get;set; }
          public IList<Inventory> Inventory { get;set; }
 
         public async Task OnGetAsync()
         {
+
+            var firstDay = DateTime.Today.AddDays(-30);
+
+            PrevOrders = await _context.Order.Where(x => x.date_placed >= firstDay).Include(x => x.Customer).ToListAsync();
             // Order = await _context.Order.AsNoTracking()
             //                             .ToListAsync();
-        Order_Inventory = await _context.Order_Inventory
+            Order_Inventory = await _context.Order_Inventory
                 .Include(o => o.Inventory)
                 .Include(o => o.Order).ToListAsync();
 
